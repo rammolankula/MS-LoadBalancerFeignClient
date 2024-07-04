54ba78899a9a0ea7af1a2407c2ab46b3d5503142
Ram <53596726+rammolankula@users.noreply.github.com>
2024-07-04 01:51:12 -0700

    Add files via upload

---
+++ FeignClientEurekaService/src/main/java/learn/ram/FeignClientEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package learn.ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FeignClientEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientEurekaServiceApplication.class, args);
+	}
+
+}
